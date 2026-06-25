0df8248e1d71cc1b42e51245b7e03f3824d74b83
Sybren <sybren.dekinderen@gmail.com>
2026-06-25 23:13:59 +0200

    Add Render deployment configuration

--- app.py
+++ app.py
@@ -618,4 +618,4 @@ def delete_source(solution_id, source_id):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
